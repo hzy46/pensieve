dfa86a7bc359485acce294154d71547729674e33
Zhiyuan He <zhiyuhe@microsoft.com>
2023-12-19 13:10:29 +0800

    fix

--- test/rl_no_training.py
+++ test/rl_no_training.py
@@ -156,7 +156,7 @@ def main():
                 a_batch.append(action_vec)
                 entropy_record = []
 
-                print "video count", video_count
+                print("video count", video_count)
                 video_count += 1
 
                 if video_count >= len(all_file_names):
